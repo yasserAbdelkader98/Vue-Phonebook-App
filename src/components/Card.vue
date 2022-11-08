8696c611563f5d73365803e837c065ffd5a87d2f
yasserAbdelkader98 <yasser.abdelkader2020@gmail.com>
2022-11-08 16:59:54 +0200

    responsive

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="width:20%" class="card border-secondary mb-3 h-25">
+  <div class="card border-secondary mb-3 h-25 mx-1 col-lg-2 col-md-3 col-sm-5 col-11">
   <h5 class="card-header">{{contact.job}}</h5>
   <div class="card-body">
     <h2 class="card-title">{{contact.name}}</h2>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container d-flex justify-content-between flex-wrap my-5">
-    <div class='w-50'>
+    <div class='col-lg-6 col-12'>
       <h1 class="py-5">PhoneBook App</h1>
       <h5 class="py-2">save your contacts with all details needed.</h5>
       <h5 class="py-2">Add, remove and modify your contacts list.</h5>
@@ -9,7 +9,7 @@
       <router-link class="btn btn-primary my-2 py-2" to="/contactus">Contact us</router-link>
 
     </div>
-    <div class='w-50'><img :src="contacts"></div>
+    <div class='col-lg-6 col-10'><img :src="contacts"></div>
 
   </div>
 </template>
